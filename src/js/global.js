49c4e1bc4a70bccf6371cf8a67b58825be283cbd
e6line <e6line@126.com>
2018-01-05 15:18:53 +0800

    updata
    updata

--- src/js/global.js
+++ src/js/global.js
@@ -44,7 +44,7 @@ module.exports = {
 					callBack()
 				}
 			});
-			$(className).addClass('also-full-height')
+			$(className).addClass('also-full-height');
 		}
 	},
 	tab: function (callBack) { // 目前只支持一个参数 index: 当选选项卡索引
@@ -56,6 +56,47 @@ module.exports = {
 
 			if(typeof callBack == "function"){
 				callBack(index)
+			}
+		});
+	},
+	msg: function (options) {
+		var btnsHtml = '';
+		for(var i = 0; i < options.btns.length; i++){
+			btnsHtml += '<a href="javascript:;" class="weui-btn weui-btn_'+ options.btns[i]['style'] +' also-msg-btn">'+ options.btns[i]['text'] +'</a>';
+		}
+
+		var _tpl = '<div class="also-page js_show" id="msgPage">'+
+						'<div class="weui-msg">'+
+							'<div class="weui-msg__icon-area"><i class="weui-icon-'+ options.icon +' weui-icon_msg"></i></div>'+
+							'<div class="weui-msg__text-area">'+
+									'<h2 class="weui-msg__title">'+ options.title +'</h2>'+
+									'<p class="weui-msg__desc">'+ options.desc +'</p>'+
+							'</div>'+
+							'<div class="weui-msg__opr-area">'+
+									'<p class="weui-btn-area">'+
+										btnsHtml +
+									'</p>'+
+							'</div>'+
+							'<div class="weui-msg__extra-area">'+
+									'<div class="weui-footer">'+
+											'<p class="weui-footer__links">'+
+												'<a href="javascript:void(0);" class="weui-footer__link"></a>'+
+											'</p>'+
+											'<p class="weui-footer__text">Copyright © 2018 ALSOLIFE</p>'+
+									'</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>';
+		console.log(_tpl)			
+		if($("#msgPage").length>0){
+			$("#msgPage").remove();
+		}
+		$("body").prepend(_tpl);
+
+		$(document).on('click', '.also-msg-btn', function () {
+			var _index = $(this).index();
+			if(typeof options.btns[_index].callBack == "function"){
+				options.btns[_index].callBack();
 			}
 		});
 	}
